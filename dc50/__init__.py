d74a4085aa0287902e5795dec1f321aebd05f426
Eric Brown <ebrown@brownmac.local>
2024-10-07 12:33:52 -0700

    try adding checks

--- dc50/__init__.py
+++ dc50/__init__.py
@@ -1,6 +1,18 @@
 import check50
+import check50.c
 
 @check50.check()
 def exists():
     """dc50.c exists"""
     check50.exists("dc50.c")
+
+@check50.check()
+def compiles():
+    """dc50.c compiles"""
+    check50.c.compile("dc50.c")
+
+@check50.check(compiles)
+def one_plus_one():
+    """adds 1 plus 1"""
+    check50.run("./dc50 1 1 +").stdout("2").exit(0)
+
