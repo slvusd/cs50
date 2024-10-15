0f8a2f37ba471dd896dfb07cea2930166770e13d
kadenisme <105736922+kadenisme@users.noreply.github.com>
2024-10-15 10:13:37 -0700

    Update __init__.py

--- palindromes/__init__.py
+++ palindromes/__init__.py
@@ -14,11 +14,7 @@ def compiles():
 @check50.check(compiles)
 def My_mom_has_a_very_level_civic.():
     """checks for palindromes"""
-  check50.run("./palindromes")
-
-@check50.check(compiles)
-def My_mom_has_a_very_level_civic.():
-    """checks for palindromes"""
+    check50.run("./palindromes")
     check50.run("My mom has a very level civic.").stdout("3").exit(0)
 
 @check50.check(compiles)
