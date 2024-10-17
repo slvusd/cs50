0ea03746464cee6c3b61c7f5976033cdf121301d
Eric Brown <ebrown@brownmac.local>
2024-10-16 21:53:01 -0700

    try lcs50=True

--- p/__init__.py
+++ p/__init__.py
@@ -9,10 +9,10 @@ def exists():
 @check50.check()
 def compiles():
     """p.c compiles"""
-    check50.run("make p")
-    #check50.c.compile("p.c")
+    #check50.run("make p")
+    check50.c.compile("p.c", lcs50=True)
 
 @check50.check(compiles)
 def test1():
     """Outputs 1"""
-    check50.run("./p").stdin("1").stdout("1").exit(0)
+    check50.run("./p").stdin("1\n").stdout("1").exit(0)
