08918160e6721a56f3f3c852cdba8cfd840d3ef4
MrBrown <ebrown@slvusd.org>
2024-10-25 13:53:25 -0700

    Update __init__.py
    escape all $ chars

--- mail/__init__.py
+++ mail/__init__.py
@@ -19,19 +19,19 @@ def one_oz_letter():
 @check50.check(compiles)
 def two_point_five_oz_letter():
     """checking price for 2.5(oz.) letter"""
-    check50.run("./mail").stdin("2.5").stdout("$1.30\n").exit(0)
+    check50.run("./mail").stdin("2.5").stdout("\$1.30\n").exit(0)
 
 @check50.check(compiles)
 def twelve_point_thirtyfour_oz_letter():
     """checking price for 12.34(oz.) letter"""
-    check50.run("./mail").stdin("12.34").stdout("$3.27\n").exit(0)
+    check50.run("./mail").stdin("12.34").stdout("\$3.27\n").exit(0)
 
 @check50.check(compiles)
 def thirteen_oz_letter():
     """checking price for 13(oz.) letter"""
-    check50.run("./mail").stdin("13").stdout("$3.40\n").exit(0)
+    check50.run("./mail").stdin("13").stdout("\$3.40\n").exit(0)
 
 @check50.check(compiles)
 def thirteen_point_zero_one_oz_letter():
     """checking price for 13.01(oz.) letter"""
-    check50.run("./mail").stdin("13.01").stdout("$3.40\n").exit(0)
+    check50.run("./mail").stdin("13.01").stdout("\$3.40\n").exit(0)
